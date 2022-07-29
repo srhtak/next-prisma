827c0029e93f235bcdeb1ff121a209a961cdda8c
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-07-29 13:24:13 +0300

    migration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Head>
+        <title>Spotify</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Home
+    </div>
+  );
 };
 
 export default Home;
