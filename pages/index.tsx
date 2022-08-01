39e2e0cedaf41134ea9ebe0d9d79276547bd8ae5
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-08-01 17:29:09 +0300

    ui layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,14 @@ const Home = () => {
         <title>Spotify</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <GradientLayout color="green">
+      <GradientLayout
+        roundImage
+        image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
+        subtitle="profile"
+        title="Serhat Ak"
+        description="15 public playlist"
+        color="green"
+      >
         <div>Home Page</div>
       </GradientLayout>
     </div>
