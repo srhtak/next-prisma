99032e877ee978844975c3f3984805e7300c2f8a
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-08-02 19:51:11 +0300

    songs table ui

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,8 +1,37 @@
+import GradientLayout from "components/GradientLayout";
+import SongsTable from "components/songsTable";
 import { validateToken } from "lib/auth";
 import prisma from "lib/prisma";
 
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "teal",
+    "gray",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const Playlist = ({ playlist }) => {
-  return <div>{playlist.name}</div>;
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="subtitle"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <SongsTable />
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
