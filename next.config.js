5adda290fe19d77598bf04b82ef0a30847dbed3d
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-08-10 00:21:43 +0300

    swc false

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
